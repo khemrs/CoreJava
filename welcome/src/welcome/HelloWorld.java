734fbea3ff5ef9de918491641c52d719eac64a32
khemrs <41816023+khemrs@users.noreply.github.com>
2022-08-04 23:20:58 -0500

    Add files via upload

---
+++ welcome/src/welcome/HelloWorld.java
@@ -0,0 +1,11 @@
+package welcome;
+//Simple java program to print something on the screen 
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World, this is my first java program! ");
+	System.out.println("Created a branch and learning git as well!");
+	}
+
+}
